e85e22e8cac53b768afe2a4efe1d3a8bd7767cb8
zack <zb01406@gmail.com>
2024-01-23 12:04:10 -0500

    more neovim config

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,9 @@
+-- set leader key
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general maps
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
