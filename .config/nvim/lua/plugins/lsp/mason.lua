e11c808bcb88bd1eba2149e592d12f10e0e9de2b
zack <zb01406@gmail.com>
2024-02-02 09:18:47 -0500

    vue lsp

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,49 +1,50 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
 
-    local mason_lspconfig = require("mason-lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "emmet_ls",
-        "gopls",
-        "marksman",
-        "intelephense",
-        "sqls",
-        "htmx",
-        "jsonls"
-      },
-      automatic_installation = true, 
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"emmet_ls",
+				"gopls",
+				"marksman",
+				"intelephense",
+				"sqls",
+				"htmx",
+				"jsonls",
+				"volar",
+			},
+			automatic_installation = true,
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", 
-        "stylua", 
-        "eslint_d",     
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"eslint_d",
+			},
+		})
+	end,
 }
