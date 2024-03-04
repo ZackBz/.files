fee30c93d1f04398b1b081dd72c4a432ef0586f6
zack <zb01406@gmail.com>
2024-03-04 18:36:38 -0500

    kickstart nvim

--- .config/nvim/lua/core/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- set leader key
-vim.g.mapleader = " "
-
-local keymap = vim.keymap
-
--- general maps
-
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("n", [[<leader>bl]], ":BufferLinePick<CR>")
